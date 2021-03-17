f5245aed3775f35ff68bad99f7dfe0c7936efee5
davidlongshore <79716264+davidlongshore@users.noreply.github.com>
2021-03-17 15:58:32 -0700

    Add files via upload

---
+++ groundClass.js
@@ -0,0 +1,17 @@
+class Ground
+{
+    constructor(x, y, width, height){
+        var option={
+            isStatic:true
+        }
+        this.bodie=Bodies.rectangle(x, y, width, height, option);
+        this.width=width;
+        this.height=height;
+        World.add(world, this.bodie);
+        }
+        display(){
+            var pos=this.bodie.position;
+            rectMode(CENTER);
+            rect(pos.x, pos.y, this.width, this.height);
+        }
+}
